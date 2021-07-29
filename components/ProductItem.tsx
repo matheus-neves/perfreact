b37bee500bf59774bcb4ac24b1926b3ae9ca6ad1
Matheus Neves <matheusneves1989@gmail.com>
2021-07-28 23:11:01 -0300

    feat: Memo

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,3 +1,11 @@
+import { memo } from "react"
+
+/**
+ * 1. Pure Functional Components
+ * 2. Renders too often
+ * 3. Re-renders with same props
+ * 4. Medium to big size
+ */
 interface ProductItemProps {
   product: {
     id: number;
@@ -6,10 +14,14 @@ interface ProductItemProps {
   }
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   )
 }
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.product, nextProps.product)
+})
